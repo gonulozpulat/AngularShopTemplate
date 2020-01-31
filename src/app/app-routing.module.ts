3addd3d61286fecd9635ad7696476a924b3b2312
gonulozpulat <gonulozpulat14@gmail.com>
2020-02-01 00:27:57 +0300

    ui updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   {path:'products',component : ProductComponent},
+  {path:'product-add-1',component : ProductComponent},
+  {path:'product-add-2',component : ProductComponent},
   {path:'',redirectTo : 'products',pathMatch:'full'},
   {path:'products/category/:categoryId',component : ProductComponent} 
 ];
